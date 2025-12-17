870d7e7d3b5018156522833290cd1fc7711d8106
arian <ariangolovin397@gmail.com>
2025-12-17 16:22:45 +0200

    calculaator 02

--- 02_ajax_calculator/src/counter.ts
+++ 02_ajax_calculator/src/counter.ts
@@ -1,9 +1,15 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
     counter = count
     element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  };
+  // element.onclick = () => setCounter(counter + 1)
+  element.addEventListener('click', () => {
+    setCounter(counter + 1)
+  });
+  element.addEventListener('click', () => {
+    console.log(counter)
+  });
+  setCounter(0);
 }
